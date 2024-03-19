7b933acc6a3310adfc2104af1e9940f1226e7013
Gustavo <gustavo.belini@hotmail.com>
2024-03-19 19:04:42 -0300

    Criado o site para o uso do modelo, feito a pagina incial e criando a pagina para predição

---
+++ app/pages/1_Predição.py
@@ -0,0 +1,14 @@
+import pickle
+import pandas as pd
+import streamlit as st
+
+st.set_page_config(
+    page_title='Previsão de Churn de clientes',
+    page_icon='img/dnc.webp'
+)
+
+st.title('Previsão de Churn')
+
+st.markdown("Métricas Necessárias para a predição:")
+
+
