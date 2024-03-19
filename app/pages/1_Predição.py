9b115ddb26f388b934576d5d489662586c84f187
Gustavo <gustavo.belini@hotmail.com>
2024-03-19 19:21:35 -0300

    Criado o botão de subir o dataset a ser previsto emontado o botão para baixar

--- app/pages/1_Predição.py
+++ app/pages/1_Predição.py
@@ -15,7 +15,21 @@ st.markdown("Por favor colocar o arquivo csv para a predição")
 with open('models/model.pkl','rb') as model_file:
     model =  pickle.load(model_file)
 
-data = st.file_uploader()
+data = st.file_uploader('Upload CSV File')
+if data:
+    df_input = pd.read_csv(data)
+    df_output = df_input.assign(
+        churn= model.predict(df_input),
+        churn_probability= model.predict_proba(df_input)[:,1]
+    )
+    
+    
+    st.markdown('Previsão do Churn')
+    st.write(df_output)
+    st.download_button(
+        label='Download CSV', data=df_output.to_csv(index=False).encode('utf-8'),
+        mime='text/csv', file_name='churn_prediction.csv'
+        )
 
  
 
